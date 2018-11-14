a9ccac049d2ea6f55ebf9839aba4d9b375ff744d
Holly Melick <holmelick@wl.k12.ia.us>
2018-11-14 14:02:57 -0600

    teacher list

---
+++ js/teachers.js
@@ -0,0 +1,38 @@
+var teacher = [
+    {
+        image:"bartleson.jpg",
+        name:"Mr. Bartleson",
+        subjects:["College Algebra","Math 2","Trigonometry"],
+        teacher_id:"1"
+    },
+    {
+        image:"czarnecki.jpg",
+        name:"Mr. Czarnecki",
+        subjects:["Gym","Strength & Conditioning"],
+        teacher_id:"2"
+    },
+    {
+        image:"smith.jpg",
+        name:"Ms. Smith",
+        subjects:["Band","Music Theory"],
+        teacher_id:"3"
+    },
+    {
+        image:"jennings.jpg",
+        name:"Ms. Jennings",
+        subjects:["English 2","College Writing","ACT Prep"],
+        teacher_id:"4"
+    },
+    {
+        image:"harvey.jpg",
+        name:"Ms. Harvey",
+        subjects:["Math 3","AP Calculus","AP Statistics"],
+        teacher_id:"5"
+    },
+    {
+        image:"hauschildt.jpg",
+        name:"Mr. Hauschildt",
+        subjects:["Marketing",],
+        teacher_id:""
+    },
+]
